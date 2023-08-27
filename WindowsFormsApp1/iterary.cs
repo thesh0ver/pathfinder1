bd4af1613068261c8c8423d7dd5b3bbc0edc60e4
thesh0ver <alonespearman@gmail.com>
2023-08-27 13:08:30 -0400

    adding iterary.cs

---
+++ WindowsFormsApp1/iterary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pathfinder
+{
+    internal class iterary
+	{
+		internal void additeraryitems(string input)
+		{
+			List<string> sidelist = new List<string>();
+            
+            //sidelist.Add(travel.testitem("Test"));
+	;
+		}
+	}
+}
