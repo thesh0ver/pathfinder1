fe241612f492aa46b9be20e1cf16940e067a9c91
tleebright <129109680+tleebright@users.noreply.github.com>
2023-10-18 12:47:21 -0400

    Add files via upload

---
+++ ArguementException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace CurrencyConverterPort
+{
+    [Serializable]
+    internal class ArguementException : Exception
+    {
+        public ArguementException()
+        {
+        }
+
+        public ArguementException(string? message) : base(message)
+        {
+        }
+
+        public ArguementException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ArguementException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
